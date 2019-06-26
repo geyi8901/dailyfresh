364b98f60fa372d138c7f4ae1a83c4365b3fa177
Jiang Ya <jiangya89_@126.com>
2019-06-27 01:30:59 +0800

    优化:商品首页静态化+数据缓存

--- dailyfresh/apps/goods/admin.py
+++ dailyfresh/apps/goods/admin.py
@@ -1,5 +1,51 @@
 from django.contrib import admin
-from goods.models import GoodsType
+from goods.models import GoodsType, IndexPromotionBanner, IndexTypeGoodsBanner, IndexGoodsBanner
+from django.core.cache import cache
 # Register your models here.
 
-admin.site.register(GoodsType)
+
+class BaseModelAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        '''新增或者更新表中数据时调用'''
+        super().save_model(request, obj, form, change)
+
+        #发出任务让celery worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        #清楚首页缓存
+        cache.delete('index_page_data')
+
+    def delete_model(self, request, obj):
+        '''删除数据时调用'''
+        super().delete_model(request, obj, form, change)
+
+        # 发出任务让celery worker重新生成首页静态页面
+        from celery_tasks.takss import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清楚首页缓存
+        cache.delete('index_page_data')
+
+
+class IndexPromotionBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class GoodsTypeAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
+admin.site.register(GoodsType, GoodsTypeAdmin)
+admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
+admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
--- dailyfresh/celery_tasks/tasks.py
+++ dailyfresh/celery_tasks/tasks.py
@@ -2,8 +2,15 @@ from celery import Celery
 from django.conf import settings
 from django.core.mail import send_mail
 import time
+
+from django.template import loader, RequestContext
+from django_redis import get_redis_connection
+from django.shortcuts import render
 import os
-import django
+# import django
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dailyfresh.settings")#这一行在wsgi.py中有初始化
+# django.setup()  #django初始化
+from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 
 
 app = Celery('celery_tasks.tasks', broker='redis://127.0.0.1:6379/8')
@@ -21,3 +28,48 @@ def send_register_active_email(to_email, username, token):
     recvier = [to_email]
     send_mail(subject, message, sender, recvier, html_message=html_message)
     time.sleep(5)
+
+@app.task
+def generate_static_index_html():
+    '''首页'''
+    # 获取商品的种类信息
+    types = GoodsType.objects.all()
+    for type in types:
+        print('*******商品分类名称:' + type.name)
+    # 获取首页轮播商品信息
+    goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+    # 获取首页促销活动信息
+    promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+    # 获取首页分类商品展示信息
+    # type_goods_banners = IndexTypeGoodsBanner.objects.all()
+    # 获取美各种累信息
+    for type in types:  # GoodsType
+        # 获取type种类首页分类商品的图片的展示信息
+        image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+        # 获取type种类首页分类商品的文字展示信息
+        title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+        # 动态给type增加属性,分别保存首页分类商品的图片展示信息和文字展示信息
+        type.image_banners = image_banners
+        type.title_banners = title_banners
+
+    # 组织模板上下文
+    context = {'types': types,
+               'goods_banners': goods_banners,
+               'promotion_banners': promotion_banners
+               }
+
+    # 使用模板
+    # 1加载模板
+    temp = loader.get_template(('static_index.html'))
+    # 2定义上下文
+    # context = RequestContext(request, context) #没有request所以忽略
+    # 模板渲染
+    static_index_html = temp.render(context)
+
+    # 生成首页静态文件
+    save_path = os.path.join(settings.BASE_DIR, 'static/index.html')
+    print(save_path)
+    with open(save_path, 'w') as f:
+        f.write(static_index_html)
